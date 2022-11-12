9b79d4e534c6d1dbc04c0b301cbafea6103e3dbd
Suuukim <toki159@naver.com>
2022-11-12 23:25:04 +0900

    day2

---
+++ day3/src/example01/FlowEx2.java
@@ -0,0 +1,16 @@
+package example01;
+
+import java.util.Scanner;
+
+public class FlowEx2 {
+	public static void main(String[] args) {
+		int input;
+		System.out.println("숫자를 하나 입력하세요 > ");
+		
+		Scanner sc = new Scanner(System.in);
+		// 1. String으로 입력받고 Integer.parseInt사용하기
+		String tmp = sc.nextLine();
+		int sum = 1;
+		
+	}
+}
