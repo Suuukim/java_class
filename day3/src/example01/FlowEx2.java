c37b7778a9acb8da997a85ede124c6ecba3332c7
Suuukim <toki159@naver.com>
2022-11-13 23:13:18 +0900

    day3

--- day3/src/example01/FlowEx2.java
+++ day3/src/example01/FlowEx2.java
@@ -10,7 +10,21 @@ public class FlowEx2 {
 		Scanner sc = new Scanner(System.in);
 		// 1. String으로 입력받고 Integer.parseInt사용하기
 		String tmp = sc.nextLine();
-		int sum = 1;
+		input = Integer.parseInt(tmp);
+		
+		//2. 바로 int로 받는 방법
+		input = sc.nextInt();
+		
+		if(input == 0) {
+			System.out.println("입력하신 숫자는 0입니다");
+		}
+		
+		if(input != 0) {
+			System.out.println("입력하신 숫자는 0이 아닙니다.");
+			System.out.println("입력하신 숫자 : " + input);
+		}
+		
+		
 		
 	}
 }
