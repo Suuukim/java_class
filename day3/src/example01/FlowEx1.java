41bc298905f5b9dc631f18434d1270e867190dae
Suuukim <toki159@naver.com>
2022-11-16 22:02:06 +0900

    day3

--- day3/src/example01/FlowEx1.java
+++ day3/src/example01/FlowEx1.java
@@ -1,5 +1,25 @@
 package example01;
 
 public class FlowEx1 {
-
+	public static void main(String[] args) {
+		int x = 0;
+		System.out.println("x = 0일 때 참인것은 ? ");
+		
+		if(x == 0 ) { // 참 (true)
+			System.out.println("x == 0");
+		}
+		
+		if(x != 0) { // 거짓 (false)
+			System.out.println("x != 0");
+		}
+		
+		if( !(x == 0) ) { // 거짓 (false)
+			System.out.println("!( x == 0)");
+		}
+		
+		if( !(x != 0)) { // 참 (true)
+			System.out.println("! (x != 0");
+		}
+		
+	}
 }
