59775f5d0a21c9f02cee90a9a4b40d3cd1ecc97b
Suuukim <toki159@naver.com>
2022-11-17 22:34:11 +0900

    day3

---
+++ day4/src/example01/ArrayEx5.java
@@ -0,0 +1,17 @@
+package example01;
+
+public class ArrayEx5 {
+	public static void main(String[] args) {
+		// 총합과 평균 구하기
+		int sum = 0;
+		double average = 0.0;
+		
+		int[] score = {100,88,97,65,42};
+
+		for(int i=0;i<score.length;i++) {
+			sum += score[i];
+		}
+		
+		
+	}
+}
