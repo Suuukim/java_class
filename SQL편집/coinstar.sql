65ee66a803ea0898653d9d0ea10fefd4fa477e07
Suuukim <toki159@naver.com>
2022-11-27 22:57:12 +0900

    coinstar

---
+++ SQL편집/coinstar.sql
@@ -0,0 +1,20 @@
+CREATE TABLE COINSTAR (
+C_ROOM NUMBER,
+C_COIN NUMBER,
+C_TIME NUMBER,
+C_PEOPLE NUMBER,
+C_SERVICE NUMBER
+);
+
+INSERT INTO COINSTAR VALUES(1, 5, 0, 2, 15);
+
+CREATE TABLE MEMBER(
+m_no number,
+m_name varchar(10),
+m_age number,
+m_gender varchar(5),
+m_address varchar(100),
+
+
+);
+
