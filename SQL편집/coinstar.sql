873a19b79448301f46a85825c2084c9c5c29dc8f
Suuukim <toki159@naver.com>
2022-11-28 00:29:19 +0900

    sql

--- SQL편집/coinstar.sql
+++ SQL편집/coinstar.sql
@@ -14,7 +14,14 @@ m_name varchar(10),
 m_age number,
 m_gender varchar(5),
 m_address varchar(100),
+);
 
-
+create table room(
+r_no number,
+r_name varchar2(100),
+r_coin number,
+r_time number,
+r_service number,
+r_message varchar2(1000)
 );
 
