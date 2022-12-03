6d331291c753af823130513bbb3e0e60aff8c870
Suuukim <toki159@naver.com>
2022-12-03 19:28:17 +0900

    day8

--- day8/src/example03/InstancedfTest.java
+++ day8/src/example03/InstancedfTest.java
@@ -1,5 +1,19 @@
 package example03;
 
 public class InstancedfTest {
-
+	public static void main(String[] args) {
+		FireEngine fe = new FireEngine();
+		
+		if(fe instanceof FireEngine) {
+			System.out.println("This is a FireEngine instance");
+		}
+		
+		if(fe instanceof Car) {
+			System.out.println("This is a Car istance");
+		}
+		
+		if(fe instanceof Object) {
+			System.out.println("This is a Object instace");
+		}
+	}
 }
