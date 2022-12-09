3131fb908c4ec1daa15feb5516885f88d0e098ee
Suuukim <toki159@naver.com>
2022-12-09 21:55:51 +0900

    java2 day08

--- day09/src/example01/PolyArgumentTest.java
+++ day09/src/example01/PolyArgumentTest.java
@@ -3,10 +3,9 @@ package example01;
 public class PolyArgumentTest {
 	public static void main(String[] args) {
 		Buyer b = new Buyer();
-		b.buy(new TvProduct());	//Product p = new TvProduct();
+		b.buy(new TvProduct());
 		System.out.println("현재 남은 돈 : " + b.money);
-		System.out.println("현재 보너스 점수 : " + b.bonusPoint);
-	
+		System.out.println("");
 	}
 }
 
@@ -14,51 +13,51 @@ class Product{
 	int price;
 	int bonusPoint;
 	
-	
-	
 	public Product(int price) {
 		this.price = price;
 		bonusPoint = (int)(price/10.0);
 	}
 }
-//
-//class TvProduct extends Product {
-//	public TvProduct() {
-//		super(100);
-//	}
-//	
-//	@Override
-//	public String toString() {
-//		return "TvProduct";
-//	}
-//}
-//
-//class ComputerProduct extends Product{
-//	public ComputerProduct() {
-//		super(200);
-//	}
-//	
-//	@Override
-//	public String toString() {
-//		return "ComputerProduct";
-//	}
-//}
 
+class TvProduct extends Product{
+	public TvProduct() {
+		super(100); 
+	}
+	
+	@Override
+	public String toString() {
+		  return "TvProduct";
+	}
+}
 
-class Buyer{		// 고객, 물건을 사는 사람
-	int money = 1000;	//보유 금액
+class ComputerProduct extends Product{
+	public ComputerProduct() {
+		super(200);
+	}
+	
+	@Override
+	public String toString() {
+		return "ComputerProduct";
+	}
+}
+
+class Buyer{			//고객, 물건을 사는 사람
+	int money = 10000;	//보유 금액
 	int bonusPoint = 0;	//보너스 금액
-
-	void buy(Product p) {
-		if(money < p.price) {
+	
+	void buy(TvProduct tv) {
+		if(money < tv.price) {
 			System.out.println("잔액이 부족하여 물건을 살 수 없습니다.");
 			return;
 		}
 		
-		money -= p.price;
-		bonusPoint += p.bonusPoint;
-		System.out.println(p + "을/를 구매하였습니다.");
+		money -= tv.price;
+		bonusPoint += tv.bonusPoint;
+		System.out.println(tv + "을/를 구매하였습니다. ");
 	}
+	
 }
+
+
 
 
