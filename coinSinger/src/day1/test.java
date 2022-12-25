fed80b44e1f6a029044c260d2ddd53a0f6aa011d
Suuukim <toki159@naver.com>
2022-12-25 23:10:42 +0900

    coinstar

--- coinSinger/src/day1/test.java
+++ coinSinger/src/day1/test.java
@@ -11,6 +11,7 @@ public class test {
 		int service = 0;	//서비스
 		int people = 0;	// 인원
 		int starttime = 0;	//들어간 시간
+		int rnum = 0; 		//룸 넘버
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("코인을 입력하세요 > ");
@@ -35,5 +36,6 @@ public class test {
 		System.out.println("입력한 인원 : " + people + "명");
 		System.out.println("입력한 서비스 : " + service);
 		System.out.println("들어간 시간 : " + starttime);
+		System.out.println("룸 번호 : " + rnum);
 	}
 }
